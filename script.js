6ac7085092ece8bafa5567c745f1755d53df95b5
swasti2611 <85633811+swasti2611@users.noreply.github.com>
2024-01-22 14:59:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ if(s2==" ")return s2;
 	return str;
 }
 
-// Do not change the code below.
-//const s1 = prompt("Enter s1:");
-//const s2 = prompt("Enter s2:");
-//alert(shortcut(s1, s2));
+Do not change the code below.
+const s1 = prompt("Enter s1:");
+const s2 = prompt("Enter s2:");
+alert(shortcut(s1, s2));
