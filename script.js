d0f786095ea64e8c7b652e216a9c5e776d36e3d6
swasti2611 <85633811+swasti2611@users.noreply.github.com>
2024-01-22 15:11:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-function shortcut(str1, str2) {
-  // your code here
 
-	
+  // your code here
+function shortcut(str1, str2) {
   if (str1.length > 0 && str2.length > 0) {
-      return str1[0].toUpperCase() + str2[0].toUpperCase();
-  } else
-  {
+    return str1[0].toUpperCase() + str2[0].toUpperCase();
+  } else {
     return '';
   }
-		
-
-	
 }
 
-Do not change the code below.
+// Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
