3666819e1d55052c208060fc90582b5ce8523f77
swasti2611 <85633811+swasti2611@users.noreply.github.com>
2024-01-22 14:58:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function shortcut(s1, s2) {
   // your code here
+if(s1==" " )return s1;
+if(s2==" ")return s2;	
+			let	str=''
+	for (let i  = 0; i  < s1.length; i ++) {
+		if(s1!=' ' && s2!= " "){
+               str+=s1.charAt(0).toUpperCase();
+			   str+=s2.charAt(0).toUpperCase();
+		}
+	}
+	return str;
 }
 
 // Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
+//const s1 = prompt("Enter s1:");
+//const s2 = prompt("Enter s2:");
+//alert(shortcut(s1, s2));
