60f295b95dac83accfb009e482bc827378081e30
swasti2611 <85633811+swasti2611@users.noreply.github.com>
2024-01-22 15:18:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,6 +16,6 @@ let str=''
 }
 
 // Do not change the code below.
-// const s1 = prompt("Enter s1:");
-// const s2 = prompt("Enter s2:");
-// alert(shortcut(s1, s2));
+const s1 = prompt("Enter s1:");
+const s2 = prompt("Enter s2:");
+alert(shortcut(s1, s2));
